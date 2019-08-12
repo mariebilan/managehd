76f95394ccd2c3ab43360a7a1d93736b3136e1cd
mariebilan <marie.bilan.cg@gmail.com>
2019-08-12 12:07:02 +0300

    Updates

---
+++ precache-manifest.97c7f346b5c7a1c7b6c82ceed0400a70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2946bad1af5af1741c679288816ed60b",
+    "url": "/managehd/index.html"
+  },
+  {
+    "revision": "220b88f9be023840fbc5",
+    "url": "/managehd/static/css/main.9f6d7b8e.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/managehd/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "220b88f9be023840fbc5",
+    "url": "/managehd/static/js/main.e4c584d4.chunk.js"
+  },
+  {
+    "revision": "7e264560e41ad72bb4e6",
+    "url": "/managehd/static/js/runtime~main.052b4be8.js"
+  }
+]);
